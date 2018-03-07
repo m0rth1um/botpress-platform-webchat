538626437e6687b24b960ec2c865b58f9ffc64df
Moritz Mackiewicz <mma@scherer-software.de>
2018-03-07 11:52:07 +0100

    Update GA id, add debug

--- src/views/web/embedded.jsx
+++ src/views/web/embedded.jsx
@@ -1,5 +1,6 @@
 import ReactGA from 'react-ga'
 import Chat from './index.jsx'
+import * as React from 'react'
 
 export default class FullscreenChat extends React.Component {
 
@@ -8,8 +9,12 @@ export default class FullscreenChat extends React.Component {
   }
 
   render() {
-    ReactGA.initialize('UA-90044826-2')
+    ReactGA.initialize('UA-115208103-1', {
+      'debug': true,
+      'cookieDomain': 'auto'
+    })
     ReactGA.event({ category: 'WebChat', action: 'Rendered Embedded Webchat', nonInteraction: true });
+    console.log(ReactGA.ga())
     return <Chat fullscreen={false} {...this.props}/>
   }
 
